3a16a768b813c93d22fba3d622b537a7d575896c
Amelia LI <jkramelia8872@gmail.com>
2023-08-14 11:05:21 -0400

    change the date to formatted date

--- client/src/components/componentA/ReviewEntry.jsx
+++ client/src/components/componentA/ReviewEntry.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+const ReviewEntry = ({ review }) => {
+  const dateObj = new Date(review.date);
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  const formattedDate = dateObj.toLocaleDateString('en-US', options);
 
-const ReviewEntry = ({review}) => {
   return (
     <div className="relative box-border h-50 w-50 p-4 border-4 reviewTile" key={review.review_id}>
-          <div className="absolute top-0 right-0 h-40 w-40" >Reviewer name: {review.reviewer_name} {review.date}</div>
-          <div>Rating: {review.rating}</div>
-          <div>Summary: {review.summary}</div>
-          <div>Recommend: {review.recommend ? 'Yes' : 'No'}</div>
-          <div>Helpfulness: {review.helpfulness}</div>
+      <div className="absolute top-0 right-0 h-50 w-50">
+        {review.reviewer_name},{formattedDate}
+      </div>
+      <div>Rating: {review.rating}</div>
+      <div>Summary: {review.summary}</div>
+      <div>Recommend: {review.recommend ? 'Yes' : 'No'}</div>
+      <div>Helpfulness: {review.helpfulness}</div>
 
-          {/* Render other review details as needed */}
+      {/* Render other review details as needed */}
     </div>
-  )
+  );
 }
 
 export default ReviewEntry;
--- client/src/components/componentA/ReviewsList.jsx
+++ client/src/components/componentA/ReviewsList.jsx
@@ -27,7 +27,6 @@ const ReviewsList = () => {
   }
 
 
-
   useEffect(() => {
     fetchReviews();
   }, []);
